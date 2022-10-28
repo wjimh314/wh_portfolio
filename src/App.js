e637d7c6b1044eef69d28f1b1fcf77b9801739d4
wjimh314 <wjimh314@gmail.com>
2022-10-28 18:17:45 -0400

    debugging/finished header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+//  import Particles from "react-tsparticles";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+
 function App() {
   return(
   <>
+  {/* {<Particles
+    params={{
+      particles:{
+        number:{
+          value:30,
+          density:{
+            enable:true,
+            value_area:900
+          }
+        }
+      }
+    }}
+  /> } */}
   <Navbar />
    <Header />
    </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import Typed from 'react-typed'
+ import Typed from 'react-typed'
 
 const Header = () => {
   return (
     <div className='header-wrapper'>
         <div className='main-info'>
             <h1>William J. Hart</h1>
-            <Typed className="typed-text"
-            strings={["Javascripts", "React","HTML5","css"]}
+            <h2>Web Developer</h2>
+            {<Typed className="typed-text"
+            strings={["Javascript", "React","HTML5","css"]}
             typedSpeed={70}
             backSpeed={70}
             loop
-            />
+            /> }
+            <a href="#" className="btn-main-offer">Contact me</a>
         </div>
     </div>
   )
